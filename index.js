279f427b13a578f744f10b09b46b652ad30645f8
TigerMuskellunge <31293449+TigerMuskellunge@users.noreply.github.com>
2018-05-14 17:59:01 -0600

    added js

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ function showSlides() {
   var i;
   var slides =
   document.getElementsByClassName("mySlides");
-}
+  var dots =
+  document.getElementsByClassName("dot");
+    for (i = 0; i < slides.lenght; i++){
+        slides[i].style.display = "none";
+    }
 slideIndex++;
 if (slideIndex > slides.length) {slideIndex = 1}
-
-slides[slideIndex-1].style.display = "block";
-setTimeout(showSlides, 2000);
+    for (i = 0; i < dots.lenght; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides
+    [slideIndex-1].style.display = "block";
+      dots[slideIndex-1].className+=" active";
+      setTimeout(showSlides, 2000);
